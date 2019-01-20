32f9aaaf589fad562e93e6f3f59944cff5423902
curdwithraisins <irinasams1@gmail.com>
2019-01-20 12:26:46 +0200

    raisin_1: add tests

--- raisins/raisin_1/test.spec.ts
+++ raisins/raisin_1/test.spec.ts
@@ -1,9 +1,19 @@
 import f from './index';
 import { expect } from 'chai';
 
-describe('test', () => {
-    it('test test', () => {
+describe('raisin_1', () => {
+    it('should return false if array is empty', () => {
+        let res = f([], 17);
+        expect(res).to.equal(false);
+    });
+
+    it('should return true if sum of two numbers in array equals k', () => {
         let res = f([10, 15, 3, 7], 17);
         expect(res).to.equal(true);
-    })
+    });
+
+    it('should return false if sum of two numbers in array doesn\'t equal k', () => {
+        let res = f([10, 15, 3, 6], 17);
+        expect(res).to.equal(false);
+    });
 });
