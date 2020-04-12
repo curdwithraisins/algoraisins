1263efc8cf01da8daf8c9250425312990cfccbd6
curdwithraisins <irinasams1@gmail.com>
2020-04-12 19:30:26 +0300

    raisin 38

--- raisins/raisin_38/index.ts
+++ raisins/raisin_38/index.ts
@@ -5,26 +5,20 @@ export const getBinaryForest = (n: number, k: number = 1) => {
     if (k > n) return [null];
     const list = [];
     for (let i = k; i <= n; i++) {
-        const root = new Node(i);
         const leftList = getBinaryForest(i - 1, k);
         const rightList = getBinaryForest(n, i + 1);
-        const a = flatten(leftList.map((l) => {
-            root.left = l;
-            return root;
-        }).map((q) => {
-            return rightList.map(r => {
-                q.right = r;
-                return q;
-            });
-        }));
-        console.log(a);
-        list.push(a);
+        list.push(...connect(i, leftList, rightList));
     }
     return list;
 };
 
-const connect = () => {
-
+const connect = (c: number, left: any, right: any): Node[] => {
+    return flatten(left.map((l) => {
+        return right.map(r => {
+            const root = new Node(c);
+            root.left = l;
+            root.right = r;
+            return root;
+        });
+    }));
 };
-
-console.log(flatten(getBinaryForest(3)));
--- raisins/raisin_38/test.spec.ts
+++ raisins/raisin_38/test.spec.ts
@@ -2,9 +2,10 @@ import { getBinaryForest } from './index';
 import { expect } from 'chai';
 
 describe('raisin_38', () => {
-    describe('should return true', () => {
-        it('return true for small number', () => {
-
+    describe('should return a list of all trees', () => {
+        it('return correct number of trees', () => {
+            const res = getBinaryForest(3);
+            expect(res.length).to.eql(5);
         });
     });
 });
