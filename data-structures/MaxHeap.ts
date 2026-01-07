44f0abaa1c0eafb6a6d69bde3b5eb57247b41362
curdwithraisins <irinasams1@gmail.com>
2026-01-06 23:31:03 -0800

    Implement Max/MinHeap

---
+++ data-structures/MaxHeap.ts
@@ -0,0 +1,84 @@
+/**
+ * A max-heap is a binary tree where each node is greater than or equal to its children.
+ * The root of the tree is the largest element in the heap.
+ * The heap is a complete binary tree, which means that all the levels of the tree are filled except possibly the last level.
+ * The last level is filled from left to right.
+ */
+
+export class MaxHeap {
+    private heap: any[];
+    constructor() {
+        this.heap = [];
+    }
+    get peek(): any {
+        return this.heap[0];
+    }
+    get size(): number {
+        return this.heap.length;
+    }
+    add(v: any): void {
+        this.heap.unshift(v);
+        this.heapifyUp();
+    }
+    delete(v: any): void {
+        const index = this.heap.indexOf(v);
+        if (index === -1) {
+            return;
+        }
+        if (index === this.heap.length - 1) {
+            this.heap.pop();
+            return;
+        }
+        this.heap[index] = this.heap.pop();
+        this.heapifyDown(index);
+        this.heapifyUp();
+    }
+    toString(): void {
+        let inLine: number[] = [0];
+        console.log(this.heap[0]);
+        while (inLine.length > 0) {
+            let newInLine = [];
+            let str = "";
+            for (let i = 0; i < inLine.length; i++) {
+                const index = inLine[i];
+                if (index === undefined) continue;
+                let left = 2 * index + 1;
+                let right = 2 * index + 2;
+                if (left < this.heap.length) {
+                    str += this.heap[left] + " ";
+                    newInLine.push(left);
+                }
+                if (right < this.heap.length) {
+                    str += this.heap[right] + " ";
+                    newInLine.push(right);
+                }
+            }
+            console.log(str);
+            inLine = newInLine;
+        }
+    }
+    private heapifyUp(): void {
+        let index = this.heap.length - 1;
+        while (index > 0 && this.heap[index] > this.heap[Math.floor((index - 1) / 2)]) {
+            let parent = Math.floor((index - 1) / 2);
+            if (this.heap[index] > this.heap[parent]) {
+                [this.heap[index], this.heap[parent]] = [this.heap[parent], this.heap[index]];
+            }
+        }
+    }
+    private heapifyDown(index: number): void {
+        let left = 2 * index + 1;
+        let right = 2 * index + 2;
+        let largest = index;
+        if (left < this.heap.length && this.heap[left] > this.heap[largest]) {
+            largest = left;
+        }
+        if (right < this.heap.length && this.heap[right] > this.heap[largest]) {
+            largest = right;
+        }
+        if (largest !== index) {
+            [this.heap[index], this.heap[largest]] = [this.heap[largest], this.heap[index]];
+            this.heapifyDown(largest);
+        }
+    }
+}
---
+++ eslint.config.mjs
@@ -0,0 +1,31 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    files: ['**/*.ts'],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': ['error', { 
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_' 
+      }],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-non-null-assertion': 'warn',
+    },
+  },
+  {
+    ignores: ['node_modules/', 'dist/', '*.js', '*.mjs'],
+  }
+);
+
